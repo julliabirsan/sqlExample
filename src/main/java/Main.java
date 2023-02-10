0748fc552901db739aedc3a1c7139d33dd7258e0
julliabirsan <julliabirsan@yahoo.com>
2023-02-10 19:54:06 +0200

    twest

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import java.sql.*;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jullia2", "root", "rootuser");
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from angajat");
+        while (resultSet.next()){
+            System.out.println(resultSet.getInt("id") + resultSet.getString("nume"));
+            if (resultSet.getString("nume").contains("birsan")){
+                Statement statement1 = connection.createStatement();
+                int result =- statement1.executeUpdate("insert into angajat values (null, 'jutest', 1)");
+            }
+        }
+    }
+}
